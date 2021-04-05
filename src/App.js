88576f2314768ed3661ab49129f78b029ee738c0
hemni28 <hemni28@gmail.com>
2021-04-05 23:12:55 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+function Food({fav}){ // 이런식으로 props object 내부를 열어서 fav를 가져올 수 있음
+    return <h2>I like {fav}</h2>;
+}
+// function Food(props){
+//     console.log(props.fav);//props.fav = {fav}랑 같은 의미
+//     return <h2>I like Potato</h2>;
+// }
+/*
+ component에 정보를 보낼 수 있음
+ React > 재사용 가능한 컴포넌트를 만들 수 있어서 계속해서 반복해서 사용할 수 있다!!
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello! :D</h1>
+        { /* 컴포넌트에 정보를 보내는 것, kimchi라는 value로 fav 이라는 property(prop)을 준 것
+            => string 뿐 아니라, boolean, array 등 상관없음
+            -> 이런 것들을 props라고 부름
+            father to the children 컴포넌트로 원하는 많은 props를 보낼 수 있음
+            => react magic이라는 것은 react가 이걸 보게 되고 react는 모든 속성을 가져옴
+            => 컴포넌트의 argument(인자)로 넣을 수 있음            */ }
+        <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
+        <Food fav="ramen"/>
+        <Food fav="samgiopsal"/>
+        <Food fav="jjukkumi"/>
+        {/* jsx + props 의 힘!
+            jsx = HTML + JavaScript
+            component는 대문자로 시작해야하고, 컴포넌트로 데이터를 보낼 수 있음
+            props = 컴포넌트에 넣게되는 것으로 father 컴포넌트에서 child 컴포넌트로 데이터를 보낼 수 있고 argument로 감*/}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// react는 component와 함께 동작! 모든 것은 component!
+// component란? HTML을 반환하는 함수 like App.js - function App(){}
+// javascript 와 HTML 사이의 조합을 JSX라고 함
+// react application은 하나의 component(즉, App)만을 rendering 해야함 >> So, 모든 컴포넌트는 application 안에 들어가야
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// react > app.js component를 root 내부에 넣어줌 >> Virtual DOM
+// application 이 처음 로딩될땐 빈 HTML 로드하고 그 후에 react가 HTML을 밀어넣음 => react가 빠르게 작동하는 이유!
