e99308e76025711fae7c522dd270af2326953dd1
hemni28 <hemni28@gmail.com>
2021-04-11 23:42:53 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -14,44 +14,24 @@ class App extends React.Component{
      */
     constructor() {
         super();
-        console.log('hello');
     }
     /*
     state : 보통 동적 데이터와 함께 작업할 때 만들어짐, 즉 변하는 데이터 or 존재하지 않는 데이터 => 이런 것은 props가 돕지 못함
+    *** setState를 호출하면 react는 state를 refresh하고 또한 render function을 호출함 (virtualDOM을 통해 필요한 부분만 update)
      */
     state = {
-        count: 0
+        isLoading: true,
+        movies: [] // 처음에 선언하는 것이 필수는 아님
     }
-    add = () => {
-        console.log('add');
-        // this.state.count = 1; //이렇게 direct로 하면 react가 render function을 refresh해주지 않음 => setState()를 해줘야함
-        this.setState({count: this.state.count + 1});
-    };
-    minus = () => {
-        console.log('minus');
-        // *** setState를 호출하면 react는 state를 refresh하고 또한 render function을 호출함 (virtualDOM을 통해 필요한 부분만 update)
-        this.setState(current => ({count: current.count - 1}));
-        // ㄴ> state를 set할 때, add()에서 처럼 외부의 상태를 가져와서 의존하는 것은 좋지 않음, current를 가져와서 사용할 수 있음
-    };
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('I just updated');
-    }
-    componentDidMount() {
-        console.log('component rendered');
-    }
-    componentWillUnmount() {
-        console.log('Goodbye~');
+    componentDidMount() { // component가 mount 되자마자 호출됨
+        setTimeout(() => {
+            this.setState({isLoading: false})}, 6000);
     }
 
     render() { // react는 자동적으로 class component의 render method를 실행함
-        console.log('I am rendering');
-        // this.add() 즉시 function 호출! this.add 클릭했을때만 호출!
+        const { isLoading } = this.state;
         return (
-        <div>
-            <h1>The number is: {this.state.count}.</h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
-        </div>
+        <div>{isLoading ? 'Loading...' : 'We are ready!'}</div>
         )
     }
 }
