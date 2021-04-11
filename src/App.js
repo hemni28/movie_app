7303a64dcf8142f60da8e03cf02c164718bf6f03
hemni28 <hemni28@gmail.com>
2021-04-11 23:34:57 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 class App extends React.Component{
     /*
+    Life Cycle : component의 대부분을 컨트롤할 수 있음
+    - Mounting
+    1) 먼저 호출되는 fucntion은 constructor()
+    2) 그리고 나서 render()
+    3) 그리고 나서 componentDidMount()
+    - Updating
+    >> 내가 변경하는 것들
+    - UnMounting
+    >> component가 죽을 때 (ex. 다른 페이지로 이동 등)
+     */
+    constructor() {
+        super();
+        console.log('hello');
+    }
+    /*
     state : 보통 동적 데이터와 함께 작업할 때 만들어짐, 즉 변하는 데이터 or 존재하지 않는 데이터 => 이런 것은 props가 돕지 못함
      */
     state = {
@@ -18,7 +33,18 @@ class App extends React.Component{
         this.setState(current => ({count: current.count - 1}));
         // ㄴ> state를 set할 때, add()에서 처럼 외부의 상태를 가져와서 의존하는 것은 좋지 않음, current를 가져와서 사용할 수 있음
     };
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('I just updated');
+    }
+    componentDidMount() {
+        console.log('component rendered');
+    }
+    componentWillUnmount() {
+        console.log('Goodbye~');
+    }
+
     render() { // react는 자동적으로 class component의 render method를 실행함
+        console.log('I am rendering');
         // this.add() 즉시 function 호출! this.add 클릭했을때만 호출!
         return (
         <div>
