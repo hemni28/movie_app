0ed44e63f8199ad85366224291b67ba7f66e266e
hemni28 <hemni28@gmail.com>
2021-04-12 23:04:26 +0900

    #4.3 Adding Genres

--- src/App.js
+++ src/App.js
@@ -51,12 +51,12 @@ class App extends React.Component{
     render() { // react는 자동적으로 class component의 render method를 실행함
         const { isLoading, movies } = this.state;
         return (
-        <section class="container">
+        <section className="container">
             {isLoading
-            ? (<div class="loader"><span class="loader__text">Loading...</span></div>)
-            : (<div class="movies">
+            ? (<div className="loader"><span className="loader__text">Loading...</span></div>)
+            : (<div className="movies">
                 {movies.map(movie => (
-                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} poster={movie.medium_cover_image} summary={movie.summary} />
+                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} poster={movie.medium_cover_image} summary={movie.summary} genres={movie.genres}/>
                 ))}
             </div>
                 )}
--- src/Movie.js
+++ src/Movie.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import './Movie.css'
 
 //state가 필요없는 경우에는 class component가 될 필요가 없고 function component로 충분
-function Movie({id, year, title, summary, poster}){
-    return <div class="movie">
+function Movie({id, year, title, summary, poster, genres}){
+    return <div className="movie">
         <img src={poster} alt={title} title={title}/>
-        <div class="movie__data">
+        <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
+            <ul className="genres">{genres.map((genre, index) => <li className="genres__genre" key={index}>{genre}</li> )}</ul>
             <p className="movie__summary">{summary}</p>
         </div>
     </div>
@@ -19,5 +20,6 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,//medium_cover_image
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 export default Movie;
